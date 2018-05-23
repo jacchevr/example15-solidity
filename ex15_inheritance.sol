a0f3e1b1a76356ed3be703ed0aa8ba282e70adae
Jacques Chevrier <34470570+jacchevr@users.noreply.github.com>
2018-05-23 13:20:48 -0600

    Update ex15_inheritance.sol

--- ex15_inheritance.sol
+++ ex15_inheritance.sol
@@ -1,1 +1,10 @@
-# example15-solidity
+pragma solidity ^0.4.21;
+
+import "https://github.com/OpenZeppelin/zeppelin-solidity/contracts/token/ERC721/ERC721Token.sol";
+
+contract MyContractToken is ERC721Token {
+    /**
+     * Here is your code
+     * 
+     */ 
+}
